a023f93f14bd3ea324ea7c42056c03669ed1852d
Nicholas Yue <yue.nicholas@gmail.com>
2022-08-29 16:31:43 -0700

    Add creation of user to run example which requires compilation

--- openfoam/openfoam_installation.sh
+++ openfoam/openfoam_installation.sh
@@ -3,3 +3,14 @@ wget -O - https://dl.openfoam.org/gpg.key > /etc/apt/trusted.gpg.d/openfoam.asc
 add-apt-repository http://dl.openfoam.org/ubuntu
 apt update
 apt install -y openfoam10
+echo ". /opt/openfoam10/etc/bashrc" >> $HOME/.bashrc
+# Create openfoam user
+# Some examples require a non-root user to build and run
+adduser --disabled-password --gecos "" openfoam
+mkdir ~openfoam/.ssh
+chown openfoam.openfoam ~openfoam/.ssh
+chmod go-rwx ~openfoam/.ssh
+cp $HOME/.ssh/authorized_keys ~openfoam/.ssh/
+chown openfoam.openfoam ~openfoam/.ssh/authorized_keys
+su - openfoam -c 'echo ". /opt/openfoam10/etc/bashrc" >> $HOME/.bashrc'
+
